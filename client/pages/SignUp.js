e8dc5b6fb8b99b2cc87ec24fff47bc3b9b55a826
Luke Robinson <asdluke1234@gmail.com>
2022-08-12 16:32:52 +0200

    Create SignUp.js

---
+++ client/pages/SignUp.js
@@ -0,0 +1,110 @@
+import { useState } from 'react'
+import { RegisterUser } from '../services/Auth'
+import { useNavigate } from 'react-router-dom'
+const SignUp = () => {
+  let navigate = useNavigate()
+  const [formValues, setFormValues] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  })
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await RegisterUser({
+      name: formValues.name,
+      email: formValues.email,
+      username: formValues.username,
+      password: formValues.password
+    })
+    setFormValues({
+      name: '',
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: ''
+    })
+    navigate('/signin')
+  }
+
+  return (
+    <div className="signin col">
+      <div className="card-overlay centered">
+        <form className="col" onSubmit={handleSubmit}>
+          <div className="input-wrapper">
+            <label htmlFor="name">Name</label>
+            <input
+              onChange={handleChange}
+              name="name"
+              type="text"
+              placeholder="John Smith"
+              value={formValues.name}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="email">Email</label>
+            <input
+              onChange={handleChange}
+              name="email"
+              type="email"
+              placeholder="example@example.com"
+              value={formValues.email}
+              required
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input
+              onChange={handleChange}
+              name="username"
+              type="text"
+              placeholder=""
+              value={formValues.username}
+              required
+            />
+          </div>
+
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="password"
+              value={formValues.password}
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              onChange={handleChange}
+              type="password"
+              name="confirmPassword"
+              value={formValues.confirmPassword}
+              required
+            />
+          </div>
+          <button
+            disabled={
+              !formValues.email ||
+              (!formValues.password &&
+                formValues.confirmPassword === formValues.password)
+            }
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default SignUp
