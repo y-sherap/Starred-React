01ce07e311018c2f0d9e52683e06f7ca46bf1fb5
Luke Robinson <asdluke1234@gmail.com>
2022-08-12 16:06:53 +0200

    Add code to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,50 @@
-import './App.css';
-
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router'
+import Nav from './components/Nav'
+import PlaylistDetails from './pages/PlaylistDetails'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import { CheckSession } from './services/Auth'
+import './styles/App.css'
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+  const checkToken = async () =>{
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if(token){
+      checkToken()
+    }
+  },[])
   return (
     <div className="App">
+       <Nav
+        authenticated={authenticated}
+        user={user}
+        handleLogOut={handleLogOut}
+      />
+      <main>
+       <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<SignIn setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard user={user} authenticated={authenticated}/>} />
+          <Route path="/playlist" element={<PlaylistDetails user={user} authenticated={authenticated}/>} />
+        </Routes>
+      </main>
     </div>
   );
 }
