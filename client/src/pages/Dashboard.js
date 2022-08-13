7b0fec4f3f965fae5450b2eb5f572bd9f3d5b516
Luke Robinson <asdluke1234@gmail.com>
2022-08-13 10:37:40 +0200

    called imports for dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,19 @@
 import Search from "../components/Search"
+import Playlist from "../components/Playlist"
+import { useState } from "react"
+import axios from "axios"
+import { useNavigate } from 'react-router-dom'
 const Dashboard = (props) => {
     return(
         <div>
+            <div>
             <Search user = {props.user}/>
+            </div>
+            <div>
+
+            </div>
         </div>
+
     )
 
 }
