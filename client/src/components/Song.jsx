cebfd19515c2aa5ce1db5de776bb10f913d2ada8
Luke Robinson <asdluke1234@gmail.com>
2022-08-13 21:12:31 +0200

    updated song component and search

--- client/src/components/Song.jsx
+++ client/src/components/Song.jsx
@@ -1,16 +1,17 @@
-import axios from 'axios'
-
+import { useState } from 'react'
+import Client from "../services/api"
 const Song = ({ song, playlists, removeFromPlaylist, inPlaylist }) => {
+  const [pickPlaylist,setPickPlaylist] = useState()
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log('submit')
     try {
-      let res = await axios.post(`/song/${e.target.value}`, {
-        title: song.title,
-        artist: song.artist.name,
-        duration: song.duration,
-        image: song.album.cover,
-        playlistId: e.target.value
+      let res = await Client.post(`/song/${pickPlaylist}`, {
+        title: song.name,
+        artist: song.artists.items[0].profile.name,
+        duration: song.duration.totalMilliseconds,
+        image: song.albumOfTrack.coverArt.sources[0].url,
+        playlistId: pickPlaylist
       })
       console.log(res)
     } catch (e) {
@@ -23,18 +24,18 @@ const Song = ({ song, playlists, removeFromPlaylist, inPlaylist }) => {
       <p>{song.name}</p>
       <p>{song.artists.items[0].profile.name}</p>
       {inPlaylist ? <p>{Math.floor(parseInt(song.duration.totalMilliseconds/60000))}</p> : <span></span>}
-      {/* {inPlaylist ? (
+      {inPlaylist ? (
         <button onClick={(song) => removeFromPlaylist(song)}>Remove</button>
       ) : (
         <form onSubmit={(e) => handleSubmit(e)}>
-          <select id="select-playlist">
+          <select onChange={(e) => setPickPlaylist(e.target.value)}id="select-playlist">
             {playlists.map((playlist) => (
               <option value={playlist.id}>{playlist.name}</option>
             ))}
           </select>
           <button type="submit">Add</button>
         </form>
-      )} */}
+      )}
     </div>
   )
 }
