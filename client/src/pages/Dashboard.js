131b5d36c765b124251333a10b5ec5744e43bf06
Luke Robinson <asdluke1234@gmail.com>
2022-08-13 11:58:08 +0200

    create code to set playlists state

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,20 +1,39 @@
 import Search from "../components/Search"
 import Playlist from "../components/Playlist"
-import { useState } from "react"
-import axios from "axios"
+import { useState,useEffect} from "react"
 import { useNavigate } from 'react-router-dom'
-const Dashboard = (props) => {
-    return(
+import Client from "../services/api"
+const Dashboard = ({user, authenticated}) => {
+    const [playlists,setPlaylists] = useState([])
+
+    const renderPlaylists = async () => {
+        try{
+            let temp = []
+            const res = await Client.get(`/playlist/all`)
+            const playlistArr = res.data
+            playlistArr.forEach((playlist)=> {
+                let tempObj = {...playlist,isEdit: false}
+                temp.push(tempObj)
+            })
+            setPlaylists(temp)
+        }catch(e){
+            console.error(e)
+        }
+    }
+    useEffect(() => {
+        renderPlaylists()
+    },[])
+
+    return(user && authenticated) ? (
         <div>
             <div>
-            <Search user = {props.user}/>
+            <Search user = {user}/>
             </div>
+            
             <div>
-
             </div>
         </div>
-
-    )
+    ) : <div><h1>Please sign in</h1></div>
 
 }
 
