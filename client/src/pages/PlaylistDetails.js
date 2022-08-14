12746101d701856238e34b7c553bb00a2a2c9b19
William Fitzgerald <willfitzy03@gmail.com>
2022-08-14 13:29:30 -0400

    changed to playlistdetails.js

--- client/src/pages/PlaylistDetails.js
+++ client/src/pages/PlaylistDetails.js
@@ -1,24 +1,24 @@
-import { useParams } from "react-router-dom"
-import { useState,useEffect } from "react"
+import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Client from "../services/api"
-import Song from "../components/Song"
+import Client from '../services/api'
+import Song from '../components/Song'
 
-const PlaylistDetails = ({user,authenticated,}) => {
-    const {name,id} = useParams()
-    const [songs,setSongs]= useState([])
+const PlaylistDetails = ({ user, authenticated }) => {
+  const { name, id } = useParams()
+  const [songs, setSongs] = useState([])
 
-    const renderSongs = async () =>{
-        const res = await Client.get(`/song/${id}`)
-        console.log(res)
-    }
-    useEffect(()=>{
-        renderSongs()
-    },[])
-    return(
-        <div>   
-            <h1>{name}</h1>
-        </div>
-    )
+  const renderSongs = async () => {
+    const res = await Client.get(`/song/${id}`)
+    console.log(res)
+  }
+  useEffect(() => {
+    renderSongs()
+  }, [])
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
 }
 export default PlaylistDetails
