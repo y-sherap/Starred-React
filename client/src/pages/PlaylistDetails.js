6e4563549d4cb5129d71eb34a055147e03bb62b8
Luke Robinson <asdluke1234@gmail.com>
2022-08-14 18:54:55 +0200

    created useEffect for page

--- client/src/pages/PlaylistDetails.js
+++ client/src/pages/PlaylistDetails.js
@@ -7,7 +7,14 @@ import Song from "../components/Song"
 const PlaylistDetails = ({user,authenticated,}) => {
     const {name,id} = useParams()
     const [songs,setSongs]= useState([])
-    
+
+    const renderSongs = async () =>{
+        const res = await Client.get(`/song/${id}`)
+        console.log(res)
+    }
+    useEffect(()=>{
+        renderSongs()
+    },[])
     return(
         <div>   
             <h1>{name}</h1>
