6e69f46b509edc1cefb1ab6c3031e4425e4398b1
Luke Robinson <asdluke1234@gmail.com>
2022-08-15 15:26:37 +0200

    Boiler for home and updated nav.jsx and app.js

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+import { useState,useEffect } from "react"
+import Client from "../services/api"
+import { useNavigate } from "react-router-dom"
+import Playlist from "../components/Playlist"
+
+const Home = ({user,authenticated}) => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default Home
