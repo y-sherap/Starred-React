ee593bcd291dff21574dab78bef09446a29f1a99
William Fitzgerald <willfitzy03@gmail.com>
2022-08-17 10:48:16 -0400

    fixed up homeplaylist home and playlist with css

--- client/src/components/HomePlaylist/HomePlaylist.jsx
+++ client/src/components/HomePlaylist/HomePlaylist.jsx
@@ -7,29 +7,21 @@ const HomePlaylist = ({ playlist, index, updateHover, isHover, user , togglePopu
 
 
   return (
-    <div className= "homePlaylistContainer" id="blur" onClick={() =>togglePopup()}>
-      {user ? (
+    <div className= "playlistCard" id="blur" onClick={() =>togglePopup()}>
+      <div className="imageHolder">
         <img
           onMouseOver={() => updateHover(true, index)}
           src={playlist.image}
           alt="playlist image"
           className="playlistImage"  
         />
-      ) : (
-        <img
-          onMouseOver={() => updateHover(true, index)}
-          src={playlist.image}
-          alt="playlist image"
-          className="playlistImage"        />
-      )}
-      {isHover ? (
-        <div>
+    </div>
+      
+        <div className='innerPlaylist'>
           <h3 className="playlistName">{playlist.name}</h3>
           <h5 className="playlistMood">{playlist.mood}</h5>
         </div>
-      ) : (
-        <span></span>
-      )}
+        
       
     </div>
   )
