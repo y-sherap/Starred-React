8211563f8b1daf193c5f43633494fef5d98c3cc8
William Fitzgerald <willfitzy03@gmail.com>
2022-08-18 20:49:43 -0400

    ok final deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 
-export const BASE_URL = 'https://starred-react.herokuapp.com/3001'
+export const BASE_URL = 'https://starred-react.herokuapp.com/'
 
 const Client = Axios.create({ baseURL: BASE_URL })
 
